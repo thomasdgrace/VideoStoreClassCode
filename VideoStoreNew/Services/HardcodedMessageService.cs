6f8b402bdc8506a2357097270fca6b900018e836
Thomas Grace <thomas.grace@wits.ac.za>
2019-02-23 11:13:37 +0200

    Chapter 1 - Services, Start up and Configure methods

---
+++ VideoStoreNew/Services/HardcodedMessageService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VideoStoreNew.Services
+{
+    public class HardcodedMessageService : IMessageService
+    {
+        public string GetMessage()
+        {
+            return "Hardcoded Message from a Service";
+        }
+    }
+}
--- VideoStoreNew/Startup.cs
+++ VideoStoreNew/Startup.cs
@@ -1,24 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using VideoStoreNew.Services;
 
 namespace VideoStoreNew
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+        public Startup()
+        {            
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(provider => Configuration );
+            services.AddSingleton<IMessageService, ConfigurationMessageService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IMessageService msg)
         {
             if (env.IsDevelopment())
             {
@@ -27,7 +41,8 @@ namespace VideoStoreNew
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+
+            await context.Response.WriteAsync(msg.GetMessage());
             });
         }
     }
